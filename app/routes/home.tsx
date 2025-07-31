c2ef541278ec32d01e70118f95c5c077e489c71f
Panvish Dowripilli <panvishd@gmail.com>
2025-08-01 01:23:32 +0530

    Updated homepage content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
 import type { Route } from "./+types/home";
+import {resumes} from "../../constants";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,11 +12,20 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume}/>
+            ))}
+          </div>
+      )}
     </section>
   </main>
 }
