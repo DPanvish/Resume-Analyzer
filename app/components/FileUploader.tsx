246b0e0277713eb8071f62887cba5e843040e49c
Panvish Dowripilli <panvishd@gmail.com>
2025-08-04 00:38:55 +0530

    FileUploader CCompleted

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -56,27 +56,40 @@ const FileUploader = ({onFileSelect}: FileUploaderProps) => {
                 <input {...getInputProps()} />
 
                 <div className="space-y-4 cursor-pointer">
-                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                        <img src="/icons/info.svg" alt="upload" className="size-20"/>
-                    </div>
+
 
 
                     {file ? (
                         // If a file is uploaded
-                        <div className="flex items-center space-x-3">
-                            <img src="/images/pdf.png" alt="pdf" className="size-10"/>
-                            <p className="text-sm font-medium text-gray-700 truncate max-w-xs">
-                                {file.name}
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                {/*This formatSize is implemented in the utils.ts which is a mathematical function*/}
-                                {formatSize(file.size)}
-                            </p>
+                        // Here stop propagation is to stop ccontinuously upload a file even already file is uploaded
+                        <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+                                <img src="/images/pdf.png" alt="pdf" className="size-10"/>
+                            <div className="flex items-center space-x-3">
+                                <div>
+                                    <p className="text-sm font-medium text-gray-700 truncate max-w-xs">
+                                        {file.name}
+                                    </p>
+                                    <p className="text-sm text-gray-500">
+                                        {/*This formatSize is implemented in the utils.ts which is a mathematical function*/}
+                                        {formatSize(file.size)}
+                                    </p>
+                                </div>
+                            </div>
+                            <button className="p-2 cursor-pointer" onClick={(e) => {
+                                // clicking on cross mark will remove the file by setting onFileSelect to null
+                                onFileSelect?.(null);
+                            }}>
+                                <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
+                            </button>
                         </div>
                     ) : (
 
                         // If a file is not uploaded
                         <div>
+                            {/*This below img block is displayed when no file is uploaded*/}
+                            <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                <img src="/icons/info.svg" alt="upload" className="size-20"/>
+                            </div>
                             <p className="text-lg text-grap-500">
                                 <span className="font-semibold">
                                     Click to upload Resume
