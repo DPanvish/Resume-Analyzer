12c1f061b5a721a28ac1173c43e97e85f9ba5662
Panvish Dowripilli <panvishd@gmail.com>
2025-08-11 19:07:22 +0530

    Changed the home page to dark mode

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router'
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navbar shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
