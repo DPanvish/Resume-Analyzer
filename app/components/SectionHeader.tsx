3920b0141c5bfc3b5c286786da764b7e215373fe
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-09-24 21:56:39 +0530

    Section Header Component Done

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -1,1 +1,20 @@
 import React from 'react';
+
+const SectionHeader = ({eyebrow, title, description, className = ''} : SectionHeaderProps) => {
+    return(
+        <div className={`section-header ${className}`}>
+            {/* Eyebrow text appears above the title */}
+            {eyebrow && <p className="section-header-eyebrow">{eyebrow}</p>}
+
+            {/* The main title, which can be a string or a complex element like a gradient text */}
+            {typeof title === "string" ? (
+                <h1>title</h1>
+            ) : (
+                title
+            )}
+
+            {/* The description text, which can be a string or a complex element like a gradient text */}
+            {description && <p className="section-header-description">{description}</p>}
+        </div>
+    )
+}
