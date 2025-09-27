d1c0c44c3411328cc33bc0225d5f11618845547f
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-09-27 22:51:40 +0530

    Empty State Component Completed

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const EmptyState = () => {
+const EmptyState = ({icon, message, cta, className = ""} : EmptyStateProps) => {
     return (
-        <div>EmptyState</div>
+        <div className={`empty-state $className`}>
+            {/* The icon or illustration for the empty state */}
+            {icon && <div className="empty-state-icon">{icon}</div>}
+
+            {/* The main message */}
+            <div className="empty-state-message">{message}</div>
+
+            {/* The call-to-action button or link */}
+            {cta && <div className="empty-state-cta">{cta}</div>}
+        </div>
     )
 }
 export default EmptyState
