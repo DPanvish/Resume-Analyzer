c23580e7fc2453908af693a906a3965e03d4071e
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-10-07 23:26:42 +0530

    Toast Functions

--- app/services/useToast.tsx
+++ app/services/useToast.tsx
@@ -1,4 +1,21 @@
-import React from 'react'
+import React, {createContext, useCallback, useState} from 'react'
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export const ToastProvider = ({children} : {children: React.ReactNode}) => {
+    const [toasts, setToasts] = useState<ToastMessage[]>([]);
+
+    // useCallback is used to ensure that the addToast function is not recreated on every render
+    const addToast = useCallback((message: string, type: ToastType) => {
+        const id = Date.now();
+
+        setToasts(prevToasts => [...prevToasts, {id, message, type}]);
+    }, []);
+
+    const removerToast = useCallback((id: number) => {
+        setToasts(prevToasts => prevToasts.filter(toast => toast.id !== id));
+    }, []);
+}
 
 const UseToast = () => {
     return (
